8282125f54597df3979f789a2e7ee08406fb74f5
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-01-13 13:48:32 +0300

    Changes

---
+++ index.js
@@ -0,0 +1,12 @@
+// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = parseInt(Math.floor(Math.random() * 100));
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(20, 19, 18, 17, 16, 15);
